340a19333bc2ad9f40614d403d5d4406ea8d5446
dekkoumi <85477646+dekkoumi@users.noreply.github.com>
2021-12-21 15:36:54 +0100

    exo distri

---
+++ CSharp/Algo/Exercice_distyributeur_de_banque/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercice_distyributeur_de_banque
+{
+    class Exercice_distributeur_de_banque
+    {
+        static void Main(string[] args)
+        {
+            string nom;
+            string prenom;
+            int mot_de_passe;
+            double montant_du_compte;
+            double derniere_transaction;
+            solde[0] =452.25 ;
+            solde[1] = 845.25;
+
+            Console.WriteLine(" Donnez le nom: ");
+            nom = (Console.ReadLine());
+            Console.WriteLine(" Donnez le prenom: ");
+            prenom = (Console.ReadLine());
+            Console.WriteLine(" quel est le mot de passe: ");
+            mot_de_passe = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Donnez le montant du compte: ");
+            montant_du_compte = double.Parse(Console.ReadLine());
+            Console.WriteLine(" Donnez la dernière transaction");
+            derniere_transaction = double.Parse(Console.ReadLine());
+            Console.WriteLine(" donnez le montant du solde");
+
+
+
+
+
+
+            
+            {
+
+            }
+            
+
+
+        }
+    }
+}
