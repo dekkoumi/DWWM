4ee729c60c2a7dc0695af896041f949d07e29985
dekkoumi <dekkoumi.s@hotmail.fr>
2022-04-14 10:12:29 +0200

    exo date_heure

--- Web/Exercice_javaScript/Exercice_javaScript2.js
+++ Web/Exercice_javaScript/Exercice_javaScript2.js
@@ -20,3 +20,26 @@ btndate.addEventListener("click", function(){
 }
 )
 // inscris moi un evenement click
+
+let monheure=new Date();
+let hh=monheure.getHours();
+let min=monheure.getMinutes();
+let sec=monheure.getSeconds();
+if(hh<10){
+    hh='0'+ hh;
+}
+if(min<10){
+    min='0'+min;
+}
+if(sec<10){
+    sec='0+'+sec;
+}
+
+let text_heure = ""+ hh +":" + min +":" + sec;
+
+
+let btn_heure = document.getElementById("btn_heure");
+btn_heure.addEventListener("click",function(){
+    document.getElementById("txt_heure").value= text_heure;
+}
+)
