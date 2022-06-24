a34671a0d98cb69728ea822995353a0919da4951
dekkoumi <dekkoumi.s@hotmail.fr>
2022-06-24 11:47:50 +0200

    exo

---
+++ Web/tp_guide_michelin/MyTable.php
@@ -0,0 +1,7 @@
+<?php
+define('USER',"root");
+define('PASSWD',"");
+define('SERVER',"localhost");
+define('BASE',"guide_duchemin");
+
+?>
--- Web/tp_pret/pret.php
+++ Web/tp_pret/pret.php
@@ -32,6 +32,27 @@ public function mensualite(){
     return round($a,2);
 }
 
+public function genererTableau(){
+    $chaine = "<table class='table table-dark table-striped' ><thead><tr><th>Numéro de <br>mois </th> <th>Par Intérêts </th><th>Partie <br>Amortissement </th><th>capital restant <br>dù </th><th>Mensualité <br>constante </th></thead><tbody>";
+    $parinteret = 0;
+    $parAmortissement = 0; 
+    $capitalRestant = 0;
+    for ($i=0; $i < $this->nb_mois; $i++) { 
+       if ($i==0) {
+        $parinteret = $this->k*$this->tm;
+        $parAmortissement = $this->Mensualite()-$parinteret;
+        $capitalRestant = $this->k-$parAmortissement;
+        $chaine.="<tr><td>".($i+1)."</td><td>".round($parinteret,2)."</td><td>".round($parAmortissement,2)."</td><td>".round($capitalRestant,2)."</td><td>".round($this->Mensualite(),2)."</td></tr>";
+       }else{
+        $parinteret = $capitalRestant*$this->tm;
+        $parAmortissement = $this->Mensualite()-$parinteret;
+        $capitalRestant = $capitalRestant-$parAmortissement;
+        $chaine.="<tr><td>".($i+1)."</td><td>".round($parinteret,2)."</td><td>".round($parAmortissement,2)."</td><td>".round($capitalRestant,2)."</td><td>".round($this->Mensualite(),2)."</td></tr>";
+       }
+    }
+    $chaine.="</tbody></table>";
+    return $chaine;
+}
 
 
 
